14e136ab47e271219a43f7c8c515b71e56147ee2
Alyona <borisovsk_a@mail.ru>
2022-12-01 15:51:02 +0300

    feat: Copy text

--- app/src/main/java/com/example/passwordapp/MainActivity.kt
+++ app/src/main/java/com/example/passwordapp/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.passwordapp
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.passwordapp.constants.Constants
+import com.example.passwordapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActLog"
+    lateinit var mainBinding: ActivityMainBinding
+
+    lateinit var myClipboard: ClipboardManager
+    lateinit var myClip: ClipData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.d(TAG, "In onCreate")
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        myClipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+
+        val enteredText = mainBinding.enterText.text
+
+        mainBinding.copyButton.setOnClickListener {
+            Log.d(TAG, "Button Copy pressed")
+            myClip = ClipData.newPlainText(Constants.COPIED_TEXT, enteredText)
+            myClipboard.setPrimaryClip(myClip)
+
+            Toast.makeText(this, R.string.text_was_copied, Toast.LENGTH_SHORT).show()
+        }
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/passwordapp/constants/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.passwordapp.constants
+
+object Constants {
+    const val COPIED_TEXT = "copied_text"
+}
