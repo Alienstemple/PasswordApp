976b561568b4013c8d1592146abefd2e3a333842
Alyona <borisovsk_a@mail.ru>
2022-12-02 11:14:30 +0300

    debug: Temp solution for NPE R - strings to constants

--- app/src/main/java/com/example/passwordapp/misc/Constants.kt
+++ app/src/main/java/com/example/passwordapp/misc/Constants.kt
@@ -2,4 +2,12 @@ package com.example.passwordapp.misc
 
 object Constants {
     const val COPIED_TEXT = "copied_text"
+    const val EN_LOW_KEYB = "qwertyuiop[]asdfghjkl;'zxcvbnm,."
+    const val EN_UP_KEYB = "QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>"
+    const val EN_LOW = "qwertyuiopasdfghjklzxcvbnm"
+    const val EN_UP = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    const val RU_LOW = "йцукенгшщзхъфывапролджэячсмитьбю"
+    const val RU_UP = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
+    const val NUM = "1234567890"
+    const val SPEC = "~!@#$%^&*-_"
 }
