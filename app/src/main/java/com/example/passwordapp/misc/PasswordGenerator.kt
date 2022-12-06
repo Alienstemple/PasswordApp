d4f760a35a5079b23e7dd1eed6067e558873c45a
Alyona <borisovsk_a@mail.ru>
2022-12-06 13:21:55 +0300

    refactor: Add at least one symbol function to passwordGenerator

--- app/src/main/java/com/example/passwordapp/misc/PasswordGenerator.kt
+++ app/src/main/java/com/example/passwordapp/misc/PasswordGenerator.kt
@@ -15,12 +15,17 @@ class PasswordGenerator(context: Context) {
 
     fun generatePassword(length: Int, up: Boolean, num: Boolean, special: Boolean): String {
         var symbolsRange = en_low  // Default - en low
-        if (up) symbolsRange =
-            "$symbolsRange${en_up}"  // TODO check min 1 symb of this category
+        if (up) symbolsRange = "$symbolsRange${en_up}"   // TODO StringBuilder
         if (num) symbolsRange = "$symbolsRange${numbers}"
         if (special) symbolsRange = "$symbolsRange${spec}"
 
-        val generatedPswd = CharArray(length) { i -> randomSymbol(symbolsRange) }.joinToString("")
+        var generatedPswd = CharArray(length) { i -> randomSymbol(symbolsRange) }.joinToString("")
+
+        // Add at least 1 symb of every category
+        if (up) replaceRandomSymbol(generatedPswd, en_up)
+        if (num) replaceRandomSymbol(generatedPswd, numbers)
+        if (special) replaceRandomSymbol(generatedPswd, spec)
+
         Log.d(TAG, "Generated: $generatedPswd")
         return generatedPswd
     }
@@ -31,5 +36,11 @@ class PasswordGenerator(context: Context) {
 
     private fun randomSymbol(symbolsRange: String): Char {
         return symbolsRange[symbolsRange.indices.random()]
+    }
+
+    private fun replaceRandomSymbol(inputString: String, targetSymbolsRange: String): String {
+        var randomIndex = inputString.indices.random()
+        var outputString = StringBuilder(inputString).also { it.setCharAt(randomIndex, randomSymbol(targetSymbolsRange)) }.toString()
+        return outputString
     }
 }
